a37dfc01041727f7789acb0d811d0f2639c83931
akhrarov-a <asad.lion06@gmail.com>
2024-12-04 03:33:40 +0500

    #configured profile

--- src/modules/profile/ui/moleculars/view/view.component.tsx
+++ src/modules/profile/ui/moleculars/view/view.component.tsx
@@ -1,5 +1,9 @@
+import { useMemo } from 'react';
+import dayjs from 'dayjs';
+import classNames from 'classnames';
 import { observer } from 'mobx-react-lite';
 import { Button } from 'antd';
+import { getMonth, getWeekday, getWeekdays } from '@utils';
 import { useStore } from '@store';
 import { useLocales } from '@locales';
 import styles from './view.module.scss';
@@ -9,10 +13,32 @@ import styles from './view.module.scss';
  */
 const View = observer(({ toggleEditing }: { toggleEditing?: () => void }) => {
   const {
+    language,
     profile: { user }
   } = useStore();
 
   const { t } = useLocales();
+
+  const userWeekends = useMemo(() => {
+    const weekends = user?.weekends || [];
+    const weekdays = getWeekdays(language);
+
+    return weekends
+      .map(weekend => weekdays[weekend === 7 ? 0 : weekend])
+      .join(', ');
+  }, [language, user]);
+
+  const userHolidays = useMemo(
+    () =>
+      user?.holidays?.map(holiday => {
+        const date = dayjs(holiday);
+        const weekday = getWeekday(date, language);
+        const month = getMonth(date, language);
+
+        return `${weekday}, ${date.date()} ${month} ${date.year()}`;
+      }) || [],
+    [user]
+  );
 
   return (
     <div className={styles.container}>
@@ -42,6 +68,23 @@ const View = observer(({ toggleEditing }: { toggleEditing?: () => void }) => {
         <div className={styles.content_field}>
           <p>{t('form.fields.email.label')}</p>
           <p>{user.email}</p>
+        </div>
+        <div className={styles.content_field}>
+          <p>{t('form.fields.weekends.label')}</p>
+          <p>{userWeekends}</p>
+        </div>
+        <div
+          className={classNames(
+            styles.content_field,
+            styles.content_field_holidays
+          )}
+        >
+          <p>{t('form.fields.holidays.label')}</p>
+          <div>
+            {userHolidays.map(holiday => (
+              <p>{holiday}</p>
+            ))}
+          </div>
         </div>
         <div className={styles.content_field}>
           <p>{t('form.fields.layoutTitle.label')}</p>
