3dc137dbfcef5b0a4ba8b02c427befec6bab1f98
akhrarov-a <asad.lion06@gmail.com>
2024-12-04 23:08:36 +0500

    #removed unused comments

--- src/hooks/use-click-outside.ts
+++ src/hooks/use-click-outside.ts
@@ -3,22 +3,20 @@ import { useEffect } from 'react';
 /**
  * Hook that alerts clicks outside of the passed ref
  */
-const useClickOutside = (ref: any, handleClickOutside: (event:any) => void) => {
+const useClickOutside = (
+  ref: any,
+  handleClickOutside: (event: any) => void
+) => {
   useEffect(() => {
-    /**
-     * Handle passed click outside
-     */
     const handleClick = (event: any) => {
       if (!(ref.current && !ref.current.contains(event.target))) return;
 
       handleClickOutside(event);
     };
 
-    // Bind the event listener
     document.addEventListener('mousedown', handleClick);
 
     return () => {
-      // Unbind the event listener on clean up
       document.removeEventListener('mousedown', handleClick);
     };
   });
